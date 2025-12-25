aa21daa5cbabadf11581822f176cafa5a5436989
weinibuliu <weinibuliu@outlook.com>
2025-12-25 15:13:24 +0800

    fix: 移除 Preview

--- composeApp/src/androidMain/kotlin/app/qrcode_share/qrcodeshare/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/app/qrcode_share/qrcodeshare/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +16,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
-}
