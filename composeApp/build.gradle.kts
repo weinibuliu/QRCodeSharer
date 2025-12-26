71aa186513297264e577d44833aad097815f8921
weinibuliu <weinibuliu@outlook.com>
2025-12-26 20:21:56 +0800

    rft: JDKTarget 11 -> 17

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 
     kotlin {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
 
